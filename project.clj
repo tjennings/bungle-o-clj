0b52413f5d5fb8f5b6142365b7ef12c422b90f73
Bodaniel Jeanes <me@bjeanes.com>
2012-05-04 20:58:00 -0500

    Fix gitignore, remove local_lib, add all dependencies to project.clj

--- project.clj
+++ project.clj
@@ -4,5 +4,7 @@
                            [ring "1.0.2"]
                            [org.clojure/clojure "1.4.0"]
                            [org.clojure/data.zip "0.1.1"]
-                           [org.clojure/data.xml "0.0.3"]]
+                           [org.clojure/data.xml "0.0.3"]
+                           [org.hornetq/hornetq-core "2.2.16.Final"]
+                           [org.jboss.netty/netty "3.2.7.Final"]]
             :dev-dependencies [[lein-beanstalk "0.2.0"]])
